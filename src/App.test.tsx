cff49ecf1bac60d8d6c61b96a6c8c685eefb15d6
Bhaskar Gogoi <bhaskargogoi24@gmail.com>
2021-05-31 16:24:09 +0200

    added unit tests for app component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,36 @@
 // src/App.test.tsx
+import { shallow, ShallowWrapper } from 'enzyme'
 import React from 'react'
-import { shallow } from 'enzyme'
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
 import App from './App'
 
+const mockStore = configureStore([])
+
 describe('<App />', () => {
-  let component
+  let store
+  let component: ShallowWrapper
 
   beforeEach(() => {
-    component = shallow(<App />)
+    store = mockStore({
+      days: {
+        days: [
+          { id: 1, label: '1 day', value: 1 },
+          { id: 2, label: '2 days', value: 2 },
+          { id: 3, label: '3 days', value: 3 },
+        ],
+        selectedDay: { id: 1, label: '1 day', value: 1 },
+      },
+    })
+    component = shallow(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
   })
-  test('It should mount', () => {
-    expect(component.length).toBe(1)
+
+  it('should render with given state from Redux store', () => {
+    expect(component).toMatchSnapshot()
   })
 })
 
